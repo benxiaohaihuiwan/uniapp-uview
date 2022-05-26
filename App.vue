ff46646582977ff1a899556ddef8f162378bcafa
严旭 <744209932@qq.com>
2022-05-26 21:04:25 +0800

    提交增加项

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// uni.navigateTo({ url: "/pages/login/login"})
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+// uView
+@import "uview-ui/index.scss";
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 学习 uniapp uview
+
+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
+
+git remote set-url origin https://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@github.com/benxiaohaihuiwan/uniapp-uview.git
+
+git remote add origin https://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@github.com/benxiaohaihuiwan/uniapp-uview.git
